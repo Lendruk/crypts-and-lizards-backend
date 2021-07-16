7cf851600cca4d31e8b79b364bbc3d1ed6adad2d
Lendruk <fenixeye3@gmail.com>
2021-07-16 23:03:37 +0100

    Added Basic Logging

---
+++ src/utils/Logger.ts
@@ -0,0 +1,18 @@
+class Logger {
+
+  private static logMessage(message: string): void {
+    console.log(message);
+  }
+
+  static info(message: string): void {
+    this.logMessage(`[Info] - ${message}`);
+  } 
+
+  static warn(message: string): void {
+    this.logMessage(`[Warn] - ${message}`);
+  } 
+
+  static error(message: string): void {
+    this.logMessage(`[Error] - ${message}`);
+  }
+}
