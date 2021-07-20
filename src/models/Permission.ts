183e975d3b85b560de1d48b98fad479207af9de4
Lendruk <fenixeye3@gmail.com>
2021-07-20 19:50:18 +0100

    Added permission and role model

---
+++ src/models/Permission.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+
+interface Permission {
+  name: string;
+}
+
+const PermissionSchema = new Schema<Permission>({
+  name: { type: String }
+}, { timestamps: { createdAt: '_created' } });
+
+const Permission = model<Permission>('Permission', PermissionSchema);
+export default Permission; 
---
+++ src/models/Role.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+import Permission from './Permission';
+
+interface Role {
+  name: string;
+  permissions: Permission[];
+}
+
+const RoleSchema = new Schema<Role>({
+  name: { type: String },
+  permissions: [{ type: Schema.Types.ObjectId, ref: 'Permission' }]
+});
+
+const Role = model<Role>('Role', RoleSchema);
+export default Role;
